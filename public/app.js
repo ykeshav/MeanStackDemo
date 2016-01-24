716cc4379c3b5418b5e47062fd7255390fe86b35
Shrukesh Daya <y.keshav1877@gmail.com>
2016-01-24 16:46:10 +0530

    created crud operation

--- public/app.js
+++ public/app.js
@@ -1,7 +1,17 @@
 var jetbrains = angular.module("jetbrains", []);
 jetbrains.controller("appController", function ($http) {
     var app = this;
-    $http.get("http://localhost:3000").success(function (products) {
-        app.products = products;
-    })
+    var url = "http://localhost:3000";
+    app.saveProduct = function (newProduct) {
+        $http.post(url + "/add", {name: newProduct}).success(function () {
+            loadProducts()
+        })
+    };
+    function loadProducts() {
+        $http.get(url).success(function (products) {
+            app.products = products;
+        })
+    }
+
+    loadProducts();
 });
--- server.js
+++ server.js
@@ -3,16 +3,35 @@
  */
 var express = require("express");
 var cors = require("cors");
+var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
+
 var app = express();
+
 app.listen(3000);
 app.use(cors());
-var mongoose = require("mongoose");
+app.use(bodyParser());
+
 mongoose.connect('mongodb://localhost/jetbrains');
 var Product = mongoose.model('Product', {name: String});
+var product = new Product({name: "webStorm"});
+
+product.save(function (err) {
+    if (err)console.log('failed');
+    else console.log('saved');
+});
 
 app.get("/", function (req, res) {
     Product.find(function (err, prodcuts) {
         res.send(prodcuts);
+    })
+});
+
+app.post("/add", function (req, res) {
+    var name = req.body.name;
+    var product = new Product({name: name});
+    product.save(function (err) {
+        res.send();
     })
 });
 
